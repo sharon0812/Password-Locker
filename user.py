9376d65270376d04ebcfa542431958362521b61c
sharon0812 <anyangosharon@gmail.com>
2020-11-21 23:45:42 +0300

    generate pasword for sring

--- user.py
+++ user.py
@@ -51,9 +51,9 @@ class Credentials:
     def display_credentials(cls):
         return cls.user_credentials
 
-# def gen_password (stringL = 10):
-#             password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "(|/~!.@,)#{?&[%]^}&*"
-#             return ''.join(random.choice(password) for i in range (stringL))
+def gen_password (stringL = 10):
+            password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "(|/~!.@,)#{?&[%]^}&*"
+            return ''.join(random.choice(password) for i in range (stringL))
 
 
 
