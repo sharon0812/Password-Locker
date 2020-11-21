0d15a8cda1831103523966f86792614dedac846d
sharon0812 <anyangosharon@gmail.com>
2020-11-21 23:36:27 +0300

    add a class method

--- user.py
+++ user.py
@@ -12,13 +12,13 @@ class User:
     def save_user(self):
         User.user_list.append(self)
 
-#     @classmethod
-#     def real_user(cls,username,password):
-#         current_user = ''
-#         for user in User.user_list:
-#             if user.username == username and user.password == password:
-#                 current_user = user.username
-#         return current_user        
+    @classmethod
+    def real_user(cls,username,password):
+        current_user = ''
+        for user in User.user_list:
+            if user.username == username and user.password == password:
+                current_user = user.username
+        return current_user        
 
 # class Credentials:
 #     user_credentials = []
